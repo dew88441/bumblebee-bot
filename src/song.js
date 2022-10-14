626c92b32122ae9a00b1b3b020a3de04cc30d15a
dew88441 <102888327+dew88441@users.noreply.github.com>
2022-10-14 09:25:57 +0700

    Add files via upload

---
+++ src/song.js
@@ -0,0 +1,22 @@
+// song object, storing song information
+// No usage at the moment.
+
+class Song
+{
+    constructor(songName, songUrl, songStream)
+    {
+        this._name = songName;
+        this._url = songUrl;
+        this.stream = songStream;
+    }
+
+    pause()
+    {
+
+    }
+
+    resume()
+    {
+
+    }
+}
